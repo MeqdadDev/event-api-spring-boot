606b217a0381dbc3a42b59382e60a3269b2b9f65
MeqdadDev <meqdad.darweesh@gmail.com>
2024-03-12 21:49:33 +0200

    add repository layer for club model

---
+++ EventAPI/src/main/java/com/meqdaddev/eventapi/repository/ClubRepository.java
@@ -0,0 +1,10 @@
+package com.meqdaddev.eventapi.repository;
+
+import com.meqdaddev.eventapi.models.Club;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface ClubRepository extends JpaRepository<Club, Long> {
+    Optional<Club> findByTitle(String url);
+}
