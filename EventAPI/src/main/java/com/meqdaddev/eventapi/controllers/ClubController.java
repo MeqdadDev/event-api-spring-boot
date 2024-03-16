d650df9a719d435049a83781906da05ead8f4a6b
MeqdadDev <meqdad.darweesh@gmail.com>
2024-03-16 05:21:40 +0200

    add create club form and save methods to club controller

--- EventAPI/src/main/java/com/meqdaddev/eventapi/controllers/ClubController.java
+++ EventAPI/src/main/java/com/meqdaddev/eventapi/controllers/ClubController.java
@@ -1,11 +1,14 @@
 package com.meqdaddev.eventapi.controllers;
 
 import com.meqdaddev.eventapi.dto.ClubDto;
+import com.meqdaddev.eventapi.models.Club;
 import com.meqdaddev.eventapi.services.ClubService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
 
@@ -19,6 +22,19 @@ public class ClubController {
         List<ClubDto> clubs = clubService.findAllClubs();
         model.addAttribute("clubs", clubs);
         return "clubs-list";
+    }
+
+    @GetMapping("/clubs/new")
+    public String createClubForm(Model model) {
+        Club club = new Club();
+        model.addAttribute("club", club);
+        return "clubs-create";
+    }
+
+    @PostMapping("/clubs/new")
+    public String saveClub(@ModelAttribute("club") Club club) {
+        clubService.saveClub(club);
+        return "redirect:/clubs";
 
     }
 }
