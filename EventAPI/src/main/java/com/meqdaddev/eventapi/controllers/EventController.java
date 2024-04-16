bd419b918fb650d1b5ce938e1f1979d69c3eb3f9
MeqdadDev <meqdad.darweesh@gmail.com>
2024-04-17 00:27:57 +0300

    create event controller and creation form endpoint

---
+++ EventAPI/src/main/java/com/meqdaddev/eventapi/controllers/EventController.java
@@ -0,0 +1,34 @@
+package com.meqdaddev.eventapi.controllers;
+
+import com.meqdaddev.eventapi.dto.EventDto;
+import com.meqdaddev.eventapi.models.Event;
+import com.meqdaddev.eventapi.services.EventService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class EventController {
+
+    @Autowired
+    private EventService eventService;
+
+    @GetMapping("/events/{clubId}/new")
+    public String createEventForm(@PathVariable("clubId") Long clubId, Model model) {
+        Event event = new Event();
+        model.addAttribute("clubId", clubId);
+        model.addAttribute("event", event);
+        return "events-create";
+    }
+
+    @PostMapping("/events/{clubId}")
+    public String createEvent(@PathVariable("clubId") Long clubId, @ModelAttribute("event") EventDto eventDto, Model model) {
+        eventService.createEvent(clubId, eventDto);
+        return "redirect:/clubs/" + clubId;
+    }
+
+}
