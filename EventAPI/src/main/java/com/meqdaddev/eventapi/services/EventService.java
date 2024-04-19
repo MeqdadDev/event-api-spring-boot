dcd337518712e6c6a9573277efe548713767d9da
MeqdadDev <meqdad.darweesh@gmail.com>
2024-04-19 11:23:50 +0300

    add findAllEvents to event service interface

--- EventAPI/src/main/java/com/meqdaddev/eventapi/services/EventService.java
+++ EventAPI/src/main/java/com/meqdaddev/eventapi/services/EventService.java
@@ -2,7 +2,10 @@ package com.meqdaddev.eventapi.services;
 
 import com.meqdaddev.eventapi.dto.EventDto;
 
+import java.util.List;
+
 public interface EventService {
     void createEvent(Long clubId, EventDto eventDto);
 
+    List<EventDto> findAllEvents();
 }
